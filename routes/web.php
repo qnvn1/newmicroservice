c63ef7b6d540fb9c232db20afee0bdf054391648
Dan Ivan E. Labin <danivanlabin@gmail.com>
2025-03-09 23:38:01 +0800

    updated

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,13 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->get('/users', 'UserController@getUsers');
-$router->get('/userslist', 'UserController@userslist');
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->get('/users', ['uses' => 'UserController@getUsers']);
+});
+
+$router->get('/users', 'UserController@index');
+$router->post('/users', 'UserController@add');
+$router->get('/users/{id}', 'UserController@show');
+$router->put('/users/{id}', 'UserController@update');
+$router->patch('/users/{id}', 'UserController@update');
+$router->delete('/users/{id}', 'UserController@delete');
